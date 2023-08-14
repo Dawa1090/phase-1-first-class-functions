72379a43e17446ba58d51d2724182a8c2e4f4197
Dawa Sherpa <dawasherpa1090@gmail.com>
2023-08-14 10:42:20 -0400

    first class

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback) {
+    callback()
+}
+
+function returnsANamedFunction(){
+    return function namedfunction(){
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function
+}
